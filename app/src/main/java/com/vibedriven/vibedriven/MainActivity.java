9a37d878e0bf30933d75d56ceb4fd0b48d722a6d
junoh <kjo05067@gmail.com>
2018-07-10 21:22:58 +0900

    한글화

--- app/src/main/java/com/vibedriven/vibedriven/MainActivity.java
+++ app/src/main/java/com/vibedriven/vibedriven/MainActivity.java
@@ -1,14 +1,33 @@
 package com.vibedriven.vibedriven;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
+import com.mapbox.android.core.permissions.PermissionsListener;
+import com.mapbox.android.core.permissions.PermissionsManager;
 import com.mapbox.mapboxsdk.Mapbox;
 import com.mapbox.mapboxsdk.maps.MapView;
+import com.mapbox.mapboxsdk.maps.MapboxMap;
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
+import com.mapbox.mapboxsdk.plugins.localization.LocalizationPlugin;
+import com.mapbox.mapboxsdk.plugins.localization.MapLocale;
+import com.mapbox.mapboxsdk.plugins.locationlayer.LocationLayerPlugin;
+import com.mapbox.mapboxsdk.plugins.locationlayer.modes.CameraMode;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.List;
 
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback, PermissionsListener {
+
+    private LinearLayout topView;
     private MapView mapView;
+    private MapboxMap mapboxMap;
+    private PermissionsManager permissionsManager;
+    private LocalizationPlugin localizationPlugin;
+    private View view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,7 +35,51 @@ public class MainActivity extends AppCompatActivity {
         Mapbox.getInstance(this, getString(R.string.mapbox_access_token));
         setContentView(R.layout.activity_main);
         mapView = (MapView) findViewById(R.id.mapView);
+        topView = (LinearLayout) findViewById(R.id.topView);
+        topView.bringToFront();
         mapView.onCreate(savedInstanceState);
+        mapView.getMapAsync(this);
+    }
+
+    // MARK: Mapbox implements
+    @Override
+    public void onMapReady(MapboxMap mapboxMap) {
+        MainActivity.this.mapboxMap = mapboxMap;
+        localizationPlugin = new LocalizationPlugin(mapView, mapboxMap);
+        localizationPlugin.setMapLanguage(MapLocale.KOREA);
+        enableLocationPlugin();
+    }
+
+    @SuppressWarnings({"MissingPermission"})
+    private void enableLocationPlugin() {
+        if(PermissionsManager.areLocationPermissionsGranted(this)) {
+            LocationLayerPlugin locationLayerPlugin = new LocationLayerPlugin(mapView, mapboxMap);
+            locationLayerPlugin.setCameraMode(CameraMode.TRACKING);
+            getLifecycle().addObserver(locationLayerPlugin);
+        } else {
+            permissionsManager = new PermissionsManager(this);
+            permissionsManager.requestLocationPermissions(this);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        permissionsManager.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    @Override
+    public void onExplanationNeeded(List<String> permissionsToExplain) {
+        Toast.makeText(this, R.string.user_location_permission_explanation, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onPermissionResult(boolean granted) {
+        if(granted) {
+            enableLocationPlugin();
+        } else {
+            Toast.makeText(this, R.string.user_location_permission_not_granted, Toast.LENGTH_LONG).show();
+            finish();
+        }
     }
 
     @Override
